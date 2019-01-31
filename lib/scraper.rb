b058f3d17fa917afbaaaff471e3186dfc737795b
Michael Soares <mikey.soares@gmail.com>
2019-01-31 18:13:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ require_relative './student.rb'
 class Scraper
 
   def self.scrape_index_page(html)
-    html = File.read('fixtures/student-site/index.html')
+    html = File.read('file:///Users/michaelsoares/.atom/.learn-ide/home/jocular-prompt-6869/oo-student-scraper-online-web-ft-011419/fixtures/student-site/index.html')
     page = Nokogiri::HTML(open(html))
     student = Student.new
     all_students =[]
