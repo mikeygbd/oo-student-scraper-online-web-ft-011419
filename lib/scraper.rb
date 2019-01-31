1f3bd9d18c8f1167e1387d0627acbb3e641d776c
Michael Soares <mikey.soares@gmail.com>
2019-01-31 18:24:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,18 +5,22 @@ require_relative './student.rb'
 
 class Scraper
 
+attr_accessor :all_students
+
   def self.scrape_index_page(html)
     html = File.read('fixtures/student-site/index.html')
     page = Nokogiri::HTML(open(html))
     student = Student.new
-    all_students =[]
+    @all_students =[]
 
     student_profile = {}
 
     page.css("roster-cards-container").each do |student|
       student_name = student.css("student-name").text
       all_students << student_profile[student.name.to_sym] = {}
+
     end
+    @all_students
   end
 
   def self.scrape_profile_page(profile_url)
