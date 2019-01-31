e02d27fc2a55908ea5870490a9a74774ea25567c
Michael Soares <mikey.soares@gmail.com>
2019-01-31 17:22:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,9 +3,9 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-@@html = File.read('fixtures/kickstarter.html')
+  
   def self.scrape_index_page(html)
-
+    html = File.read('fixtures/kickstarter.html')
   end
 
   def self.scrape_profile_page(profile_url)
