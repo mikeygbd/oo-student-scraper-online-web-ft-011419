f8c9c3d8bc9cb933b08234a1094176d6c89465d6
Michael Soares <mikey.soares@gmail.com>
2019-01-31 19:54:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,11 +8,12 @@ class Student
   student_hash.each_pair {|k,v| self.send(("#{k}="), v)}
 
     @@all << self
-  
+
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |student_hash|
+      self.new(student_hash)
   end
 
   def add_student_attributes(attributes_hash)
