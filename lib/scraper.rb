01ea84d3b68c4d41bb7aa044af343da03e32f9a8
Michael Soares <mikey.soares@gmail.com>
2019-01-31 19:57:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,6 @@ attr_accessor :students, :attributes
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
     attributes = {}
-
     container = doc.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
       container.each do |link|
         if link.include?("twitter")
