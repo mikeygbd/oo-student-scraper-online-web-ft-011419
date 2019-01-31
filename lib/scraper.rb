cd06af7360d5f23d33a38eb2927d8b86c782bee3
Michael Soares <mikey.soares@gmail.com>
2019-01-31 19:32:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,9 +8,9 @@ class Scraper
 attr_accessor :students
 
  def self.scrape_index_page(index_url)
-   page = Nokogiri::HTML(open(index_url))
+   doc = Nokogiri::HTML(open(index_url))
    students = []
-   page.css(".student-card").each do |student|
+   doc.css(".student-card").each do |student|
      name = student.css(".student-name").text
      location = student.css(".student-location").text
      profile_url = student.css("a").attribute("href").value
@@ -23,6 +23,29 @@ attr_accessor :students
   end
 
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+    students = {}
+
+    # doc.css("").each do |student|
+    #   name = student.css(".student-name").text
+
+    #   container = doc.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
+    # container.each do |link|
+    #   if link.include?("twitter")
+    #     student[:twitter] = link
+    #   elsif link.include?("linkedin")
+    #     student[:linkedin] = link
+    #   elsif link.include?("github")
+    #     student[:github] = link
+    #   elsif link.include?(".com")
+    #     student[:blog] = link
+    #   end
+    # end
+    student[:profile_quote] = page.css(".profile-quote").text
+    student[:bio] = page.css("div.description-holder p").text
+    student
+end
+
 
   end
 
