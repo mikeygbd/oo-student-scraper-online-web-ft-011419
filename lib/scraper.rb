fb53989af5b289851a91bb11f35c498057d18789
Michael Soares <mikey.soares@gmail.com>
2019-01-31 19:09:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
 
 attr_accessor :all_students
 
-  def self.scrape_index_page(index_url)
+  def self.scrape_index_page(index_url = "fixtures/student-site/index.htm" )
     html = open('index_url')
     doc = Nokogiri::HTML(open(html))
     student_cards = doc.css(".student_card")
