ba203b51630b0687ed5c7d5c468c05f431a71c63
Michael Soares <mikey.soares@gmail.com>
2019-01-31 19:14:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,23 +7,39 @@ class Scraper
 
 attr_accessor :all_students
 
-  def self.scrape_index_page(index_url)
-    index_url = open('fixtures/student-site/index.html')
-    doc = Nokogiri::HTML(open(index_url))
-    student_cards = doc.css(".student_card")
-    all_students =[]
-    students = {}
-    student_cards.collect do |student_card|
-      [students] => {
-        :name => student_card.css("h4.student-name").text,
-        :location => student_card.css("p.student-location").text,
-        :profile_url => "./fixtures/student-site/" + student_card.css("a").attribute("href").value
-        }
-        all_students << students
+  # def self.scrape_index_page(index_url)
+  #   index_url = open('fixtures/student-site/index.html')
+  #   doc = Nokogiri::HTML(open(index_url))
+  #   student_cards = doc.css(".student_card")
+  #   all_students =[]
+  #   students = {}
+  #   student_cards.collect do |student_card|
+  #     [students] => {
+  #       :name => student_card.css("h4.student-name").text,
+  #       :location => student_card.css("p.student-location").text,
+  #       :profile_url => "./fixtures/student-site/" + student_card.css("a").attribute("href").value
+  #       }
+  #       all_students << students
+  #
+  #   end
+  #   all_students
+  # end
 
-    end
-    all_students
-  end
+  def self.scrape_index_page(index_url)
+
+    index_html = open(index_url)
+   index_doc = Nokogiri::HTML(index_html)
+   student_cards = index_doc.css(".student-card")
+   students = []
+   student_cards.collect do |student_card_xml|
+     students << {
+       :name => student_card_xml.css("h4.student-name").text,
+       :location => student_card_xml.css("p.student-location").text,
+       :profile_url => "./fixtures/student-site/" + student_card_xml.css("a").attribute("href").value
+       }
+   end
+   students
+ end	  end
 
   def self.scrape_profile_page(profile_url)
 
