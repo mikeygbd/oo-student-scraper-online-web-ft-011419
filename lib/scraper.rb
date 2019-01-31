97d169d6a72f811642eceaf46e6cb5d0fee2eca0
Michael Soares <mikey.soares@gmail.com>
2019-01-31 19:21:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,21 +25,37 @@ attr_accessor :students
   #   all_students
   # end
 
-  def self.scrape_index_page(index_url)
+ #  def self.scrape_index_page(index_url)
+ #
+ #    index_html = open(index_url)
+ #   index_doc = Nokogiri::HTML(index_html)
+ #   student_cards = index_doc.css(".student-card")
+ #   students = []
+ #   student_cards.collect do |student_card_xml|
+ #     students << {
+ #       :name => student_card_xml.css("h4.student-name").text,
+ #       :location => student_card_xml.css("p.student-location").text,
+ #       :profile_url => "./fixtures/student-site/" + student_card_xml.css("a").attribute("href").value
+ #       }
+ #   end
+ #   students
+ # end
 
-    index_html = open(index_url)
-   index_doc = Nokogiri::HTML(index_html)
-   student_cards = index_doc.css(".student-card")
+ def self.scrape_index_page(index_url)
+   page = Nokogiri::HTML(open(index_url))
    students = []
-   student_cards.collect do |student_card_xml|
-     students << {
-       :name => student_card_xml.css("h4.student-name").text,
-       :location => student_card_xml.css("p.student-location").text,
-       :profile_url => "./fixtures/student-site/" + student_card_xml.css("a").attribute("href").value
-       }
-   end
+
+   page.css("div.student-card").each do |student|
+     name = student.css(".student-name").text
+     location = student.css(".student-location").text
+     profile_url = student.css("a").attribute("href").value
+     student_info = {:name => name,
+               :location => location,
+               :profile_url => profile_url}
+     students << student_info
+     end
    students
- end
+  end
 
   def self.scrape_profile_page(profile_url)
 
