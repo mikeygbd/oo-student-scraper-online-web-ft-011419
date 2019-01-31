61d8602a73d3f6baf6a63dd10ef741238dcb9677
Michael Soares <mikey.soares@gmail.com>
2019-01-31 18:49:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
 attr_accessor :all_students
 
   def self.scrape_index_page(index_url)
-      # binding.pry
+      binding.pry
     index_url = open('fixtures/student-site/index.html')
     page = Nokogiri::HTML(open(index_url))
     # student = Student.new
@@ -18,7 +18,7 @@ attr_accessor :all_students
     page.css("roster-cards-container student-name").each do |student|
 
       student_name = student.css("h4.student-name").text
-      binding.pry
+      # binding.pry
       location = student.css("p.student-location").text
       all_students << student_profile[student.name.to_sym] = location
 
