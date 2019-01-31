a3ea11a52f34251e2403e9c480ce8aad5294b23c
Michael Soares <mikey.soares@gmail.com>
2019-01-31 19:21:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -45,7 +45,7 @@ attr_accessor :students
    page = Nokogiri::HTML(open(index_url))
    students = []
 
-   page.css("div.student-card").each do |student|
+   page.css(".student-card").each do |student|
      name = student.css(".student-name").text
      location = student.css(".student-location").text
      profile_url = student.css("a").attribute("href").value
